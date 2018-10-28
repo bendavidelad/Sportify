5c76efb4a662929ecb59900ab2361fd5b3e4ec73
asawaed <atefwsawaed@gmail.com>
2018-10-28 19:30:53 +0200

    try

--- app/src/main/java/com/android/example/sportify/MainActivity.java
+++ app/src/main/java/com/android/example/sportify/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // comment
         //Orwa's comment
+        // Atef's comment
     }
 
     @Override
