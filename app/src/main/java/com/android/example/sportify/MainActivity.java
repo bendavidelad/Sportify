1dd52fd6387034e684072bf7fd506fe832ea286b
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2018-11-01 20:52:51 +0200

    NEWS COMES!

--- app/src/main/java/com/android/example/sportify/MainActivity.java
+++ app/src/main/java/com/android/example/sportify/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,27 +13,54 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String News_URL;
+    private String prefixUrl;
+    private String postfixUrl;
     private List<ArticlesItem> articles;
+    private Map<String, List<ArticlesItem>> allArticles;
+    private final String BASEBALL = "baseball";
+    private final String BASKETBALL = "basketball";
+    private final String FOOTBALL = "football";
+    private final String SOCCER = "soccer";
+    private final String SWIMMING = "swimming";
+    private final String JUDO = "judo";
+    private final String BOXING = "boxing";
+    private final String TENNIS = "tennis";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        News_URL = "https://newsapi.org/v2/everything?sources=ynet&q=%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C&apiKey=74c30136f8a043699c626830c267dc87";
-        new AsyncHttpTask().execute(News_URL);
+        allArticles = new HashMap<>();
+        allArticles.put(BASEBALL, new ArrayList<ArticlesItem>());
+        allArticles.put(BASKETBALL, new ArrayList<ArticlesItem>());
+        allArticles.put(FOOTBALL, new ArrayList<ArticlesItem>());
+        allArticles.put(SOCCER, new ArrayList<ArticlesItem>());
+        allArticles.put(SWIMMING, new ArrayList<ArticlesItem>());
+        allArticles.put(JUDO, new ArrayList<ArticlesItem>());
+        allArticles.put(BOXING, new ArrayList<ArticlesItem>());
+        allArticles.put(TENNIS, new ArrayList<ArticlesItem>());
 
-        System.out.println("------------------------------------------------------");
-        for(ArticlesItem article : articles) {
-            System.out.println(article.getTitle());
 
-        }
-        System.out.println("------------------------------------------------------");
+        prefixUrl = "https://newsapi.org/v2/everything?q=";
+        postfixUrl = "&apiKey=74c30136f8a043699c626830c267dc87";
+
+        AsyncHttpTask asyncHttpTask = new AsyncHttpTask();
+        asyncHttpTask.execute(prefixUrl, postfixUrl);
+//        try {
+//            asyncHttpTask.get();
+//        } catch (Exception exp){
+//            System.out.println(exp.getStackTrace());
+//        }
+
     }
 
     public void sendNewsCategory(View view) {
@@ -43,31 +68,31 @@ public class MainActivity extends AppCompatActivity {
         String newsCategory = "";
         switch(view.getId()){
             case R.id.baseballButton:
-                newsCategory = "baseball";
+                newsCategory = allArticles.get(BASEBALL).get(0).getTitle();
                 break;
             case R.id.basketballButton:
-                newsCategory = "basketball";
+                newsCategory = allArticles.get(BASKETBALL).get(0).getTitle();
                 break;
             case R.id.soccerButton:
-                newsCategory = "soccer";
+                newsCategory = allArticles.get(SOCCER).get(0).getTitle();
                 break;
             case R.id.boxingButton:
-                newsCategory = "boxing";
+                newsCategory = allArticles.get(BOXING).get(0).getTitle();
                 break;
             case R.id.swimmingButton:
-                newsCategory = "swimming";
+                newsCategory = allArticles.get(SWIMMING).get(0).getTitle();
                 break;
             case R.id.tennisButton:
-                newsCategory = "tennis";
+                newsCategory = allArticles.get(TENNIS).get(0).getTitle();
                 break;
             case R.id.footballButton:
-                newsCategory = "football";
+                newsCategory = allArticles.get(FOOTBALL).get(0).getTitle();
                 break;
             case R.id.judoButton:
-                newsCategory = "judo";
+                newsCategory = allArticles.get(JUDO).get(0).getTitle();
                 break;
         }
-        intent.putExtra("news Category", newsCategory);
+        intent.putExtra("title", newsCategory);
         startActivity(intent);
     }
 
@@ -76,20 +101,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... urls) {
             String result = "";
-            URL url;
-            HttpURLConnection urlConnection = null;
+            for (String currentArticleCategory : allArticles.keySet()){
+                String urlToSend = urls[0]  + currentArticleCategory + urls[1];
+                URL url;
+                HttpURLConnection urlConnection = null;
 
-            try {
-                url = new URL(urls[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                String response = streamToString(urlConnection.getInputStream());
-                JsonExctractor jsonExtractor = new JsonExctractor();
-                articles = jsonExtractor.parseResult(response);;
-                return result;
-            } catch (Exception e) {
-                e.printStackTrace();
+                try {
+                    url = new URL(urlToSend);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    String response = streamToString(urlConnection.getInputStream());
+                    JsonExctractor jsonExtractor = new JsonExctractor();
+                    allArticles.put(currentArticleCategory, jsonExtractor.parseResult(response));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            return null;
+            return result;
         }
     }
 
--- app/src/main/java/com/android/example/sportify/NewsActivity.java
+++ app/src/main/java/com/android/example/sportify/NewsActivity.java
@@ -1,13 +1,19 @@
 package com.android.example.sportify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
         setContentView(R.layout.activity_news);
+        TextView textView = findViewById(R.id.newsTitle);
+        textView.setText(title);
     }
 }
