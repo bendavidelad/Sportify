d5d3e0e82eea1123faf7b377dd9544a0f2ef37ea
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2018-11-01 19:34:30 +0200

    news category button click

--- app/src/main/java/com/android/example/sportify/MainActivity.java
+++ app/src/main/java/com/android/example/sportify/MainActivity.java
@@ -12,19 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // comment
-        //Orwa's comment
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-
-    public void baseballNews(View view) {
+    public void sendNewsCategory(View view) {
         Intent intent = new Intent(this, NewsActivity.class);
+        String newsCategory = "";
+        switch(view.getId()){
+            case R.id.baseballButton:
+                newsCategory = "baseball";
+                break;
+            case R.id.basketballButton:
+                newsCategory = "basketball";
+                break;
+            case R.id.soccerButton:
+                newsCategory = "soccer";
+                break;
+            case R.id.boxingButton:
+                newsCategory = "boxing";
+                break;
+            case R.id.swimmingButton:
+                newsCategory = "swimming";
+                break;
+            case R.id.tennisButton:
+                newsCategory = "tennis";
+                break;
+            case R.id.footballButton:
+                newsCategory = "football";
+                break;
+            case R.id.judoButton:
+                newsCategory = "judo";
+                break;
+        }
+        intent.putExtra("news Category", newsCategory);
         startActivity(intent);
     }
 }
