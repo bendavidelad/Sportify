83b23cb30dd3f4b268366e9ba22cda34d6432a47
asawaed <atefwsawaed@gmail.com>
2018-11-01 19:51:11 +0200

    Added the HTTP request API & JSON Extractor

--- app/src/main/java/com/android/example/sportify/MainActivity.java
+++ app/src/main/java/com/android/example/sportify/MainActivity.java
@@ -1,20 +1,41 @@
 package com.android.example.sportify;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String News_URL;
+    private List<ArticlesItem> articles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // comment
-        //Orwa's comment
+        News_URL = "https://newsapi.org/v2/everything?sources=ynet&q=%D7%9B%D7%93%D7%95%D7%A8%D7%92%D7%9C&apiKey=74c30136f8a043699c626830c267dc87";
+        new AsyncHttpTask().execute(News_URL);
+
+        System.out.println("------------------------------------------------------");
+        for(ArticlesItem article : articles) {
+            System.out.println(article.getTitle());
+
+        }
+        System.out.println("------------------------------------------------------");
     }
 
     @Override
@@ -22,9 +43,46 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
     }
 
-
     public void baseballNews(View view) {
         Intent intent = new Intent(this, NewsActivity.class);
         startActivity(intent);
     }
+
+    public class AsyncHttpTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                String response = streamToString(urlConnection.getInputStream());
+                JsonExctractor jsonExtractor = new JsonExctractor();
+                articles = jsonExtractor.parseResult(response);;
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    String streamToString(InputStream stream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        String data;
+        String result = "";
+        while ((data = bufferedReader.readLine()) != null) {
+            result += data;
+        }
+        if (stream != null) {
+            stream.close();
+        }
+        return result;
+    }
+
+
+
 }
